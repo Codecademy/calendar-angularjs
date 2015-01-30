8e1517a4f43393c712b5708728c5b24bf699d3d2
lpolepeddi <lalith@codecademy.com>
2015-01-30 16:02:50 -0500

    Initial commit

---
+++ js/controllers/DayController.js
@@ -0,0 +1,3 @@
+app.controller('DayController', ['$scope', function($scope) {
+
+}]);
---
+++ js/controllers/EventController.js
@@ -0,0 +1,3 @@
+app.controller('EventController', ['$scope', '$routeParams', function($scope, $routeParams) {
+  
+}]);
